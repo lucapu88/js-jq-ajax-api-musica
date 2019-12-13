a7af7ff4754b3b072889b9208f8619451001f364
lucapu88 <caputoluca88@gmail.com>
2019-12-13 13:17:37 +0100

    inzio

---
+++ main.js
@@ -0,0 +1,3 @@
+// Eseguire una chiamata ajax all'api di boolean: https://flynn.boolean.careers/exercises/api/array/music che restituisce una lista di 10 dischi musicali.
+// Utilizzando handlebars, disegnare in pagina una card per ogni disco.
+// BONUS: aggiungere una tendina con i generi: pop, rock, metal e jazz, che permette all'utente di filtrare i dischi visualizzati in base al genere selezionato.
